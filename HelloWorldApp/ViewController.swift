ca9f46d4b5b46ea127292cb9bed02477bb5ce3d6
Ramil Zamaletdinov <Ramil Zamaletdinov>
2022-02-28 20:51:32 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Рамиль Замалетдинов on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
